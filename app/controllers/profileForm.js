87ca721eaceae01bd84b9c2a6b5e35e703f79fe3
Jon <jon@iglu.in.th>
2016-01-09 00:06:33 +0700

    form, delete skill and insertUser function (only with console.log)

--- app/controllers/profileForm.js
+++ app/controllers/profileForm.js
@@ -1,6 +1,9 @@
-angular.module("activities");
+angular.module("activities")
     .controller("profileFormController", function ($scope) {
-        console.log("I am in personalController :)")
+        console.log("I am in personalController :)");
+
+        $scope.user = {};
+
         $scope.skill = "";
         $scope.skillsArray = [];
         $scope.addskill = function () {
@@ -8,12 +11,43 @@ angular.module("activities");
             console.log($scope.skill)
             $scope.skillsArray.push($scope.skill)
             $scope.skill = "";
-        }
+        };
 
         $scope.skillsArray = [];
 
         $scope.deleteSkill = function (index) {
-            console.log(":)" + index + ":)");
-        }
-                    
+            $scope.skillsArray.splice(index, 1);
+        };
+
+        $scope.createUser = function () {
+            if(!checkForm()) {
+                return;
+            }
+
+            $scope.user.skills = $scope.skillsArray;
+            console.log($scope.user);
+            console.log("Save this user in the databse");
+
+            clearForm();
+        };
+
+        var clearForm = function() {
+            $scope.user = {};
+            $scope.skillsArray = [];
+        };
+
+        var checkForm = function() {
+            if (!$scope.user.name) {
+                console.log("You have to fill the name");
+                return false;
+            }
+
+            if(!$scope.email) {
+                console.log("You have to fill the email");
+                return false;
+            }
+
+            console.log("everything was ok in validation");
+            return true;
+        };
     });
